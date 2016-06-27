54e3d33a3a99f06a68f2fc7a6d3682f5cf20df84
Efthemi Hatzis <ehatz01@gmail.com>
2016-06-27 17:44:39 -0400

    added buttons for rock paper scissors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,26 +8,53 @@ var config = {
 
 firebase.initializeApp(config);
 var dbRef = firebase.database().ref();
-dbRef.child('player1').set(''); //every time someone leaves or refreshes the page the values in player 1 and 2 will be reset
-dbRef.child('player2').set('');
+dbRef.child('player1').set({
+	playerName: '',
+	playerWins: '',
+	playerLosses: '',
+	playerChoice: ''
+}); //every time someone leaves or refreshes the page the values in player 1 and 2 will be reset
+dbRef.child('player2').set({
+	playerName: '',
+	playerWins: '',
+	playerLosses: '',
+	playerChoice: ''
+});
 var valueInP1; //will be set to the current values set on the database for player 1 and 2 
 var valueInP2;
 var localUser1 = ''; //will store the value of player 1 and 2 locally
 var localUser2 = '';
+
+function addRpsButtons(player) {
+	var choices = ['ROCK', 'PAPER', 'SCISSORS'];
+	for (var i = 0; i < choices.length; i++) {
+		var buttons = $('<button>', {
+		text: choices[i],
+		id: choices[i]
+	});
+		$('#' + player).append(buttons);
+	};
+	
+};
 $('#addUserBtn').on('click', function() {
 	if (!valueInP1 && !valueInP2) { 	//if there is currently no player1 and 2 the value entered in the text box will be set to player 1 and stored in the localUser1 var
 		localUser1 = $('#userNameInput').val();
-		dbRef.child('player1').set(localUser1); //resets the value on the database to the value in localUser1
+		dbRef.child('player1').child('playerName').set(localUser1); //resets the value on the database to the value in localUser1
 		$('#userNameForm').html('Hi ' + localUser1 + ', you are Player 1!');
+		addRpsButtons('player1');
+
 	} else if (valueInP1 && !valueInP2) { 	//if there is currently a player 1 and no player 2 the value entered in the text box will be set to player 2 and stored in the localUser2 var
 		localUser2 = $('#userNameInput').val();
-		dbRef.child('player2').set(localUser2); //resets the value on the database to the value in localUser1
+		dbRef.child('player2').child('playerName').set(localUser2); //resets the value on the database to the value in localUser1
 		$('#userNameForm').html('Hi ' + localUser2 + ', you are Player 2!');
+		addRpsButtons('player2');
+
 	} else if (!valueInP1 && valueInP2) {	// if there is currently no player1 and there is a player 2 the value entered in the text box will be set to player 1 and stored in the localUser1 var
 		localUser1 = $('#userNameInput').val();
-		dbRef.child('player1').set(localUser1);
+		dbRef.child('player1').child('playerName').set(localUser1);
 		$('#userNameForm').html('Hi ' + localUser1 + ', you are Player 1!');
-	}
+		addRpsButtons('player1');
+	};
 	$('#userNameInput').val(''); //resets the text inside the text box
 	return false;
 });
@@ -35,24 +62,29 @@ $('#addUserBtn').on('click', function() {
 //automatically runs at when the page loads and sets the values in the 
 //database of player 1 and 2 to the variable valueInP1 and valueInP2. this will also run everytime a value is changed on the database
 dbRef.on('value', function(snapshot) { 
-	valueInP1 = snapshot.val().player1;
-	valueInP2 = snapshot.val().player2;
-
+	valueInP1 = snapshot.val().player1.playerName;
+	console.log(valueInP1);
+	valueInP2 = snapshot.val().player2.playerName;
+	console.log(valueInP2);
 	//once this page is refreshed or opened it will automatically run this function again, 
 	//so if another user closes the page, the values stored in the current users sessions will be pushed to the database so the current user wont lose their session
 	if (localUser1) { //if there is a value in localUser1 change the value of player1 in the database (each player will only have one of the variables, the other 1 will remain undefined locally)
-		dbRef.child('player1').set(localUser1); 
+		dbRef.child('player1').child('playerName').set(localUser1); 
+	
 	} else if (localUser2) { //if there is a value in localUser2 change the value of player2 in the database 
-		dbRef.child('player2').set(localUser2);
+		dbRef.child('player2').child('playerName').set(localUser2);
 	}
 
 	if (!valueInP1) {
 		$('#p1BoxTitle').html('Waiting For Player 1...');
+	
 	} else if (valueInP1) {
 		$('#p1BoxTitle').html(valueInP1);
 	}
+	
 	if (!valueInP2) {
 		$('#p2BoxTitle').html('Waiting For Player 2...');
+	
 	} else if (valueInP2) {
 		$('#p2BoxTitle').html(valueInP2);
 	}
